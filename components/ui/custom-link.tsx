6e59d6e4160185cc44fee8163984bf6863fec62d
Rey Stone <reyhstone@gmail.com>
2026-04-04 21:04:53 -0600

    updated npm. made className universal in custom-link

--- components/ui/custom-link.tsx
+++ components/ui/custom-link.tsx
@@ -5,27 +5,22 @@ interface LinkProps {
   children: React.ReactNode;
 }
 
+const className =
+  "external-link underline decoration-1 decoration-dotted decoration-zinc-400 transition-colors duration-300 hover:text-zinc-950 hover:decoration-zinc-950 dark:hover:text-zinc-50 dark:hover:decoration-zinc-50";
+
 const isExternalLink = (href: string) =>
   href.startsWith("http://") || href.startsWith("https://") || href.startsWith("//");
 
 export function CustomLink({ href, children }: LinkProps) {
   if (isExternalLink(href)) {
     return (
-      <Link
-        href={href}
-        className="external-link underline decoration-1 decoration-dotted decoration-zinc-400 transition-colors duration-300 hover:text-zinc-950 hover:decoration-zinc-950 dark:hover:text-zinc-50 dark:hover:decoration-zinc-50"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <Link href={href} className={className} target="_blank" rel="noopener noreferrer">
         {children}
       </Link>
     );
   }
   return (
-    <Link
-      href={href}
-      className="external-link underline decoration-1 decoration-dotted decoration-zinc-400 transition-colors duration-300 hover:text-zinc-950 hover:decoration-zinc-950 dark:hover:text-zinc-50 dark:hover:decoration-zinc-50"
-    >
+    <Link href={href} className={className}>
       {children}
     </Link>
   );
