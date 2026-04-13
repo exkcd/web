95a33507d23a981f648354dfe442e6b09cb46139
Rey Stone <reyhstone@gmail.com>
2026-04-12 23:24:38 -0600

    i broke the notes folder because it didnt exist

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 import { ComponentPropsWithoutRef } from "react";
 import type { MDXComponents } from "mdx/types";
 import { Aside, Ref, Footnotes } from "@/components/layout/aside";
 import { AsideProvider } from "@/components/layout/aside-context";
 import { TooltipEmail } from "@/components/layout/tooltip-email";
 import { CustomLink } from "@/components/ui/custom-link";
+
 
 const Cover = ({ src, alt, caption }: { src: string; alt: string; caption: string }) => (
   <figure>
@@ -20,6 +22,13 @@ const Img = ({ src, alt, caption }: { src: string; alt: string; caption: string 
   </figure>
 );
 
+const RefLink = ({href, children}: {href: string; children: string}) => (
+  <Link href={href} className="group relative inline-flex shrink-0 items-center gap-1.5 rounded-full bg-zinc-100 px-2.5 py-1 text-xs text-black transition-colors duration-200 hover:bg-zinc-200 hover:text-zinc-950 dark:bg-zinc-900 dark:text-zinc-100 dark:hover:bg-zinc-800 no-underline" target="blank" rel="noopener noreferrer">
+    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-link-icon lucide-link"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/></svg>
+    {children}
+  </Link>
+)
+
 const Wrapper = ({ children }: { children: React.ReactNode }) => <AsideProvider>{children}</AsideProvider>;
 
 export const mdxComponents: MDXComponents = {
@@ -30,6 +39,7 @@ export const mdxComponents: MDXComponents = {
   Ref,
   Footnotes,
   TooltipEmail,
+  RefLink,
   a: CustomLink,
   p: ({ children }: ComponentPropsWithoutRef<"p">) => (
     <p className="mb-4 text-zinc-600 dark:text-zinc-400">{children}</p>
