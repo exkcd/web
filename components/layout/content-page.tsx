a77f44ead81943bdb42602fa495d1898b72edc92
Rey Stone <reyhstone@gmail.com>
2026-05-15 16:23:37 -0600

    removed syntax highlighting

--- components/layout/content-page.tsx
+++ components/layout/content-page.tsx
@@ -3,9 +3,7 @@ import { getContentBySlug, ContentType } from "@/lib/content";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { Toc } from "@/components/layout/toc";
 import { getTableOfContents } from "@/lib/toc";
-import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
-import rehypePrettyCode from "rehype-pretty-code";
 import Link from "next/link";
 
 interface Props {
@@ -21,12 +19,8 @@ export async function ContentPage({ type, slug }: Props) {
     <div className="prose prose-gray dark:prose-invert prose-h1:text-base prose-h1:font-medium prose-h2:text-base prose-h2:font-medium prose-h3:text-base prose-h3:font-medium prose-strong:font-medium">
       <article>
         <Link href="/" className="no-underline">
-          <div className="text-base mb-0 text-zinc-800 dark:text-zinc-300">
-            {post.title}
-          </div>
-          <div className="text-xs text-zinc-400 dark:text-zinc-600">
-            {post.date}
-          </div>
+          <div className="text-base mb-0 text-zinc-800 dark:text-zinc-300">{post.title}</div>
+          <div className="text-xs text-zinc-400 dark:text-zinc-600">{post.date}</div>
         </Link>
         <div className="text-zinc-600 dark:text-zinc-400">
           <MDXRemote
@@ -35,10 +29,6 @@ export async function ContentPage({ type, slug }: Props) {
             options={{
               mdxOptions: {
                 remarkPlugins: [remarkGfm],
-                rehypePlugins: [
-                  rehypeSlug,
-                  [rehypePrettyCode, { theme: "tokyo-night" }],
-                ],
               },
             }}
           />
